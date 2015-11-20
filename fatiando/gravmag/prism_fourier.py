7882871d0dd798a144838cf72d1869154492bb99
Andy Kass <drandykass@users.noreply.github.com>
2015-11-20 12:34:35 -0700

    Theory docs updated.

--- fatiando/gravmag/prism_fourier.py
+++ fatiando/gravmag/prism_fourier.py
@@ -8,9 +8,9 @@ Calculate the Fourier Domain expression of the potentials of a 3D right-rectangu
 .. note:: The coordinate system of the input parameters is x -> North,
     y -> East, and z -> Down.
 
-**Gravity**
+**Background**
 
-The gravitational fields are calculated based on the transform of the integral
+The potential fields are calculated based on the transform of the integral
 equation solution.  This is similar, but not identical, to the derivation by
 Bhattacharyya and Navolio (1976).
 
@@ -26,9 +26,28 @@ with
 
 and :math:`F[\cdot]` denoting the Fourier Transform.
 
-.. math:: \tilde{\phi}=\frac{\exp{-(h-z)\omega_r}{\omega_r}
+.. math:: \tilde{\phi}=\frac{e^{-(h-z)\omega_r}}{\omega_r}
 
 where :math:`\omega_r` is the radial wavenumber.
+
+
+Generalisation to a prism involves solving the volume integral over the prism.  For a prism centred horizontally at the origin with :math:`\nu\in[-a,a]`, :math:`\eta\in[-b,b]`, and :math:`\zeta\in[h_1,h_2]`, where :math:`[\nu,\eta,\zeta]` are the Cartesian coordinates of an element inside the volume of a 3D prism, the potential is given by
+
+.. math::
+
+    \tilde{\phi}(\omega_x,\omega_y,z)&=\displaystyle\int_{h_1}^{h_2}{\int_{-b}^{b}{\int_{-a}^{a}{\frac{e^{-(z-\zeta)\omega_r}}{\omega_r}e^{-i\omega_x\nu}e^{-i\omega_y\eta}~d\nu~d\eta~d\zeta}}}\\
+   &=4ab\text{sinc}\left(\frac{a\omega_x}{\pi}\right)\text{sinc}\left(\frac{b\omega_y}{\pi}\right)\frac{e^{-(h_1-z)\omega_r}-e^{-(h_2-z)\omega_r}}{\omega_r^2}
+
+To generalise for a prism in an arbitrary location, let :math:`2a` be the width of the prism in the :math:`x` direction and :math:`2b` be the width in the :math:`y` direction.  Let the horizontal coordinate of the centre of the prism be given by :math:`[\nu_0,\eta_0]`.  The potential then becomes
+
+.. math::
+   =4ab\text{sinc}\left(\frac{a\omega_x}{\pi}\right)\text{sinc}\left(\frac{b\omega_y}{\pi}\right)\frac{e^{-(h_1-z)\omega_r}-e^{-(h_2-z)\omega_r}}{\omega_r^2}e^{-i\omega_x\nu_0}e^{-i\omega_y\eta_0}.
+
+Available functions are:
+
+* :func:`~fatiando.gravmag.prism_fourier.general_potential`
+
+**Gravity**
 
 The expressions for the gravity and magnetic fields, as well as their
 gradients, can be derived by multiplication by the appropriate Fourier-Domain
@@ -36,17 +55,14 @@ operator.  Thus
 
 .. math::
     
-    \tilde{g_z}=\omega_r\tilde{\phi}
-
-    \tilde{g_x}=i\omega_x\tilde{\phi}
+    \tilde{g_z}&=\gamma\rho\omega_r\tilde{\phi}\\
+    \tilde{g_x}&=\gamma\rho i\omega_x\tilde{\phi}
 
 and so forth.
 
-Generalization to a prism involves solving the volume integral over the prism.
-
 Available functions are:
 
-* :func:`~fatiando.gravmag.prism_fourier.potential`
+* :func:`~fatiando.gravmag.prism_fourier.gravity_potential`
 * :func:`~fatiando.gravmag.prism_fourier.gx`
 * :func:`~fatiando.gravmag.prism_fourier.gy`
 * :func:`~fatiando.gravmag.prism_fourier.gz`
@@ -57,7 +73,7 @@ Available functions are:
 * :func:`~fatiando.gravmag.prism_fourier.gyz`
 * :func:`~fatiando.gravmag.prism_fourier.gzz`
 
-**Magnetic**
+**Magnetics**
 
 Available fields are the total-field anomaly and x,y,z, components of the 
 magnetic induction:
@@ -80,7 +96,7 @@ import numpy as np
 import transform
 from ..constants import G, SI2EOTVOS, CM, T2NT, SI2MGAL
 
-def potential(xp, yp, zp, prisms, dens=None):
+def gravity_potential(xp, yp, zp, prisms, dens=None):
     """
     Calculates the gravitational potential in the Fourier Domain.
 
@@ -89,13 +105,8 @@ def potential(xp, yp, zp, prisms, dens=None):
 
     .. note:: All input and output values in **SI** units(!)!
 
-    Fourier domain expression of potential for a right-rectangular prism
-    with center at the xy origin, x extent of [-a,a], y [-b,b], and z [h1,h2],
-    is given by
-    4*G*rho*a*b*sinc(a*Wx/pi)*sinc(b*Wy/pi)*(exp(-(h1-z)Wr)-exp(-(h2-z)Wr))/
-        (Wr^2)
-    where Wr is the radial wavenumber.
-
+    Computes the gravitational potential of a right rectangular 3D prism
+    in the Fourier Domain.
 
     Parameters:
 
@@ -136,6 +147,36 @@ def potential(xp, yp, zp, prisms, dens=None):
         z1, z2 = prism.z1, prism.z2
 
 
+    return 42
+
+def general_potential(xp,yp,zp,x1,x2,y1,y2,z1,z2):
+    """
+    Calculates the generalised potential in the fourier domain
+
+    .. note:: The coordinate system of the input parameters is to be
+        x -> North, y-> East, and z -> Down
+
+    This function calculates the potential response of a right rectangular
+    prism in the Fourier domain.  This is the generalised potential; to 
+    convert to gravitational potential, for example, multiply by the 
+    density and the gravitational constant.  This function is the base function
+    for computing all the other gravitational and magnetic responses.
+
+    Parameters:
+
+    * x1,x2,y1,y2,z1,z2 : floats
+        Floats with the edge coordinates of the prism to compute
+    * xp,yp,zp : arrays
+        Arrays with the x, y, and z coordinates of the computation points
+
+    Returns:
+
+    * res_space : array
+        Resulting array of potentials in the space domain
+    * res_fourier : complex array
+        Resulting array of potentials in the Fourier domain
+
+    """
 
 
     return 42
